9d64d992b77f5adf3f60525222e661b1b5645465
kimga000 <oo11267@naver.com>
2026-06-29 21:31:51 +0900

    [Fix] 콘솔 로그 자동 제거 코드 수정

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/SpotBalance/",
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: env.VITE_KEEP_CONSOLE !== "true", // 콘솔 로그 삭제
+        drop_debugger: true, // 디버거 삭제
+      },
+    },
+  },
 });
